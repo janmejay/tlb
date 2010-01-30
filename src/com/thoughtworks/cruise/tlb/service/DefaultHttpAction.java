6df6bd679c3a6e958213d86c082d3b6f321028e7
Janmejay Singh <singh.janmejay@gmail.com>
2010-01-30 01:45:34 -0800

    doesn't make unnecessary assumptions to use http insteed of https

--- src/com/thoughtworks/cruise/tlb/service/DefaultHttpAction.java
+++ src/com/thoughtworks/cruise/tlb/service/DefaultHttpAction.java
@@ -7,8 +7,6 @@ import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.params.HttpClientParams;
 
 import java.io.IOException;
-import java.net.URL;
-import java.net.MalformedURLException;
 
 import com.thoughtworks.cruise.tlb.utils.SystemEnvironment;
 import static com.thoughtworks.cruise.tlb.TlbConstants.USERNAME;
@@ -33,7 +31,6 @@ public class DefaultHttpAction implements HttpAction {
     }
 
     public String get(String url) {
-        url = httpUrl(url);
         GetMethod method = new GetMethod(url);
         try {
             int result = client.executeMethod(method);
@@ -46,24 +43,6 @@ public class DefaultHttpAction implements HttpAction {
             return method.getResponseBodyAsString();
         } catch (IOException e) {
             throw new RuntimeException("Oops! Something went wrong", e);
-        }
-    }
-
-    //HACK! Replace this with actual https support.
-    private String httpUrl(String url) {
-        try {
-            URL url1 = new URL(url);
-            String baseUrl = url;
-            if (url1.getProtocol().equals("https")) {
-                baseUrl = String.format("http://%s:%s%s", url1.getHost(), url1.getPort() - 1, url1.getPath());
-                if (url1.getQuery() != null && !url1.getQuery().isEmpty()) {
-                    baseUrl = baseUrl + "?" + url1.getQuery();
-                }
-            }
-            System.out.println("Using url = " + baseUrl);
-            return baseUrl;
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
         }
     }
 
