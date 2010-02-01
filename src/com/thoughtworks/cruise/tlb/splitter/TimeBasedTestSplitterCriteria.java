20b5ad3cf2873ce4066a2d6a700d82dedd597164
Janmejay Singh <singh.janmejay@gmail.com>
2010-01-31 23:55:00 -0800

    refactored towards a superclass TestSplitterCriteria, so time based criteria can use it

--- src/com/thoughtworks/cruise/tlb/splitter/TimeBasedTestSplitterCriteria.java
+++ src/com/thoughtworks/cruise/tlb/splitter/TimeBasedTestSplitterCriteria.java
@@ -11,12 +11,12 @@ import com.thoughtworks.cruise.tlb.utils.SystemEnvironment;
 /**
  * @understands criteria for splitting tests based on time taken
  */
-public class TimeBasedTestSplitterCriteria implements TestSplitterCriteria {
-    private final SystemEnvironment env;
+public class TimeBasedTestSplitterCriteria extends TestSplitterCriteria {
 
     public TimeBasedTestSplitterCriteria(SystemEnvironment env) {
-        this.env = env;
+        super(env);
     }
+
     public List<FileResource> filter(List<FileResource> files) {
         throw new RuntimeException("Not yet implemented");
     }
