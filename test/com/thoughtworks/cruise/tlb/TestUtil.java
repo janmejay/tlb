ae3f0031b49f1b509a50e7e34b645a593e6cf377
Janmejay Singh <singh.janmejay@gmail.com>
2010-03-12 12:53:42 +0530

    added logging tests for retry logic

--- test/com/thoughtworks/cruise/tlb/TestUtil.java
+++ test/com/thoughtworks/cruise/tlb/TestUtil.java
@@ -1,15 +1,92 @@
 package com.thoughtworks.cruise.tlb;
 
-import org.apache.tools.ant.types.resources.FileResource;
-import org.apache.tools.ant.Project;
-
-import java.util.*;
-import java.io.File;
-
-import com.thoughtworks.cruise.tlb.utils.SystemEnvironment;
 import com.thoughtworks.cruise.tlb.utils.FileUtil;
+import com.thoughtworks.cruise.tlb.utils.SystemEnvironment;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.types.resources.FileResource;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.util.*;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 public class TestUtil {
+
+    public static class LogFixture {
+        private ArrayList<Logger> loggersRegisteredTo;
+        private TestUtil.LogFixture.TestHandler handler;
+
+        public void assertHeard(String partOfMessage) {
+            assertHeard(partOfMessage, 1);
+        }
+
+        public void assertHeard(String partOfMessage, int expectedOccurances) {
+            int actualOccurances = totalOccurances(partOfMessage);
+            assertThat(String.format("log message '%s' should have been heard %s times, but was actually heard %s times in %s statements",
+                    partOfMessage, expectedOccurances, actualOccurances, handler.messages.size()), actualOccurances, is(expectedOccurances));
+        }
+
+        private int totalOccurances(String partOfMessage) {
+            int numberOfOccurances = 0;
+            for (String message : handler.messages) {
+                if (message.contains(partOfMessage)) numberOfOccurances++;
+            }
+            return numberOfOccurances;
+        }
+
+        class TestHandler extends Handler {
+            private ArrayList<String> messages;
+
+            TestHandler() {
+                messages = new ArrayList<String>();
+            }
+
+            public void publish(LogRecord record) {
+                messages.add(record.getMessage());
+            }
+
+            public void flush() {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+
+            public void close() throws SecurityException {
+                //To change body of implemented methods use File | Settings | File Templates.
+            }
+        }
+
+        public LogFixture() {
+            loggersRegisteredTo = new ArrayList<Logger>();
+            handler = new TestHandler();
+        }
+
+        public void startListening() {
+            populateLoggers();
+            for (Logger logger : loggersRegisteredTo) {
+                logger.addHandler(handler);
+            }
+        }
+
+        private void populateLoggers() {
+            Enumeration<String> activeLoggers = LogManager.getLogManager().getLoggerNames();
+            while(activeLoggers.hasMoreElements()) {
+                String loggerName = activeLoggers.nextElement();
+                if (!loggerName.isEmpty()) {
+                    loggersRegisteredTo.add(Logger.getLogger(loggerName));
+                }
+            }
+        }
+
+        public void stopListening() {
+            for (Logger logger : loggersRegisteredTo) {
+                logger.removeHandler(handler);
+            }
+        }
+    }
     public static List<FileResource> files(int ... numbers) {
         ArrayList<FileResource> resources = new ArrayList<FileResource>();
         for (int number : numbers) {
